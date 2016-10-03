cb9406189bf88698116a4d09e72cb66eeb577a03
Aldo Sorbara <aldo1891@gmail.com>
2016-10-03 08:44:58 -0300

    zoom fix

--- js/script.js
+++ js/script.js
@@ -2,14 +2,9 @@ $(window).load(function() {
 	var valorx="";
 	var valory="";
 	var zoom1;
-	if (screen.width<992) {
+	if (screen.width>991) {
 		zoom1 = new ch.Zoom(ch('#zoom-default')[0],{
-			side: 'bottom',
-			align: 'center'
+		
 		});
-	} 
-	else{
-		zoom1 = new ch.Zoom(ch('#zoom-default')[0],{
-		});
-	};
+	}
 });
